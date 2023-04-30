36161360f52cc6ed35d6ca0404a0b4f61814c3fb
Wallace Romualdo <67033167+WallaceRomualdoJF@users.noreply.github.com>
2023-04-30 16:45:00 -0300

    Atualização Diagrama e Interface

---
+++ src/main/java/org/example/wallace/IMontagemComputadorFacade.java
@@ -0,0 +1,7 @@
+package org.example.wallace;
+
+public interface IMontagemComputadorFacade {
+    public void ligarComputador();
+    public void desligarComputador();
+    public void executarTarefa(String tarefa);
+}
